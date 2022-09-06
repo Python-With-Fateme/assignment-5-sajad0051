a8f50ff577dd1d6eeba51ca22afca15101e71829
sajjad <sajjadmahmodi051@gmail.com>
2022-09-06 21:17:39 +0430

    assignment5

--- as5-1.py
+++ as5-1.py
@@ -1,1 +1,4 @@
 # fact 6 = 6*5*4*3*2*1
+a=input('enter your add ?')
+b=len(a)
+print(b)
--- as5-2.py
+++ as5-2.py
@@ -1,1 +1,9 @@
-
+while True:
+    add1=float(input('enter your number? '))
+    add2=float(input('enter your number? '))
+    if add1==0 and add2==0:
+        break
+    if add1>add2:
+        print('add1 is bigger')
+    else:
+        print('add2 is bigger')
